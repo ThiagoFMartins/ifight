72d08f4d728ad1331d34ea9b97abbe1348d7f0cf
ThiagoFMartins <teagufarias@gmail.com>
2017-11-08 09:17:01 -0200

    Orientado a OO

--- App.py
+++ App.py
@@ -1,5 +1,53 @@
 import sqlite3
 
+class Usuario():
+    def __init__(self, nome, email, senha, profissao, sexo, data_nasc):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.profissao = profissao
+        self.sexo = sexo
+        self.data_nasc = data_nasc
+
+    def inserir(self):
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+          INSERT INTO TB_Usuario (nome, email, senha, profissao, sexo, data_nasc)
+          VALUES (self.nome, self.email, self.senha, self.profissao,
+           self.sexo, self.data_nasc)
+        ''')
+        conn.commit()
+        conn.close()
+
+
+    def listar(self):
+        usuarios = []
+        conn = sqlite3.connect('dinamics.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT * FROM TB_Usuario;
+        """)
+        for linha in cursor.fetchall():
+            nome = linha[1]
+            email = linha[2]
+            senha = linha[3]
+            profissao = linha[4]
+            sexo = linha[5]
+            data_nasc = linha[6]
+            usuario = Usuario(nome, email, senha, profissao, sexo, data_nasc)
+            usuarios.append(usuario)
+
+        conn.close()
+
+        return usuarios
+
+    def deletar(self, id):
+        pass
+
+    def atualizar(self, nome, email, senha, profissao, sexo, data_nasc):
+        pass
+
 conn = sqlite3.connect('ifight.db')
 
 cursor = conn.cursor()
