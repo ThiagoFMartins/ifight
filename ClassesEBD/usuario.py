b045ea3c1c6be3afaae8cb0e7bca69ad295499be
Thiago Farias <thiago.martins.farias99@gmail.com>
2017-11-28 21:57:20 -0200

    Amigo
    Criando função para adicionar amigos

--- ClassesEBD/usuario.py
+++ ClassesEBD/usuario.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('ifight.db')
 cursor = conn.cursor()
 
 class usuario():
-    def __init__(self,id,login,senha,nome,nascimento,profissao,genero,mensagem):
+    def __init__(self,id,login,senha,nome,nascimento,profissao,genero,mensagem,amigos):
         self.id = id
         self.login = login
         self.senha = senha
@@ -13,13 +13,14 @@ class usuario():
         self.profissao = profissao
         self.genero = genero
         self.mensagem = mensagem
+        self.amigos = amigos
 
     def inserir(self):
         conn = sqlite3.connect('ifight.db')
         cursor = conn.cursor()
         cursor.execute("""
-        insert into tb_ususario(login,senha,nome,nascimento,profissao,genero,mensagem)
-        values(self.login,self.senha,self.nome,self.nascimento,self.profissao,self.genero,self.mensagem)
+        insert into tb_ususario(login,senha,nome,nascimento,profissao,genero,mensagem,amigos)
+        values(self.login,self.senha,self.nome,self.nascimento,self.profissao,self.genero,self.mensagem,self.amigos)
         """)
         conn.commit()
         conn.close()
@@ -42,7 +43,8 @@ class usuario():
             profissao = linha[5]
             genero = linha[6]
             mensagem = linha[7]
-            usuario = usuario(login,senha,nome,nascimento,profissao,genero,mensagem)
+            amigos = linha[8]
+            usuario = usuario(login,senha,nome,nascimento,profissao,genero,mensagem,amigos)
             usuarios.append(usuario)
 
         conn.close()
@@ -60,3 +62,6 @@ class usuario():
 
         conn.commit()
         conn.close()
+
+    def adicionarAmigo(self):
+        amigos.inserir()
